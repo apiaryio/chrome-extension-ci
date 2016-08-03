80d023a282dd16476f4d74ca514d03faab52ad67
Adam Kliment <adam@apiary.io>
2016-08-03 14:19:07 -0700

    All the initial goodness.

---
+++ README.md
@@ -0,0 +1,40 @@
+# Chrome Extension in Continuous Integration
+[![Build Status](https://travis-ci.org/apiaryio/chrome-extension-ci.svg?branch=master)](https://travis-ci.org/apiaryio/chrome-extension-ci)
+
+
+## The Stack
+
+- [Node.js](https://nodejs.org/en/) - Programming Language
+- [Mocha](https://mochajs.org/) - Node.js test frameworks
+- [Chai](http://chaijs.com/) - Node.js test assertion/framework
+- [Webdriver.io](http://webdriver.io/) - Node.js bindings and DSL for Selenium
+- [selenium-standalone](https://github.com/vvo/selenium-standalone) - Node.js wrapper runnnig and fetching the Standalone selenium server and its dependencies (ChromeFriver etc...)
+- [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/capabilities) -Selenium driver for commanding Chrome
+- [xvfb](https://docs.travis-ci.com/user/gui-and-headless-browsers/) (for headless Chrome in the CI) - Virtaul frame buffer for X server
+- [Travis CI](https://travis-ci.org) - the CI
+
+
+## How to TDD the unpacked chrome extension locally
+
+
+- Install NPM dependecies
+```
+$ npm install
+```
+
+- Install Selenium Drivers
+```
+$ npm run selenium-install
+```
+
+- Spin up the Standalone Selenium Server
+```
+$ npm run selenium-start
+```
+
+- Run the tests suite
+```
+$ npm test
+```
+
+
--- test/integration/webdriverio-test.js
+++ test/integration/webdriverio-test.js
@@ -1,30 +1,79 @@
-var assert = require('chai').assert
-var webdriverio = require('webdriverio')
-var options = { desiredCapabilities: { browserName: 'chrome' } };
+var fs = require('fs');
+var assert = require('chai').assert;
+var webdriverio = require('webdriverio');
+
+var options = {
+  desiredCapabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [
+         "--load-extension=./"
+       ]
+    }
+  }
+};
+
 var client = webdriverio.remote(options);
+
+getExtensionName = function(){
+  manifestFile = 'manifest.json'
+  manifest = JSON.parse(fs.readFileSync(manifestFile).toString())
+  return(manifest['name'])
+}
+
+getExtensionId = function(browserInstance, callback){
+  extensionName = getExtensionName()
+  var extensionIndex = null;
+  browserInstance
+    .url("chrome://extensions")
+    .waitForExist('iframe[name=extensions]')
+    .frame("extensions")
+    .click('#toggle-dev-on')
+    .getText('h2.extension-title')
+    .then(function(extensionTitles){
+      extensionIndex = extensionTitles.indexOf(extensionName)
+    }).getText('.extension-id')
+    .then(function(extensionIds){
+      extensionId = extensionIds[extensionIndex];
+      callback(undefined, extensionId)
+    })
+}
 
 describe('Webdriver.io', function(){
   this.timeout(60000);
+
   beforeEach(function(done){
     client.init().call(done)
   });
+
   afterEach(function(done){
     client.end().call(done)
   });
 
-  describe('when I go to google', function(){
-    it('the page should have google\'s title', function(done){
+  describe ('when I go to the extension page', function(){
+    it('the page should have the extension name listed', function(done){
+      var extensionName = getExtensionName()
       client
-        .url("https://google.com")
-        .getTitle()
-        .then(function(title){
-          assert.equal(title, 'Google')
+        .url("chrome://extensions")
+        .waitForExist('iframe[name=extensions]')
+        .frame("extensions")
+        .click('#toggle-dev-on')
+        .getText('h2.extension-title')
+        .then(function(extensionTitles){
+          assert.include(extensionTitles, extensionName)
         })
         .call(done)
     });
+
+    it('I should be able to get the extension id', function(done){
+      getExtensionId(client, function(err, extensionId){
+        assert.lengthOf(extensionId, 32);
+        done();
+      })
+    });
   });
 
-  describe('when I execute an async JS code', function(){
+  describe('when I execute a JS code', function(){
     it('should return result', function(done){
 
       fn = function(number1, number2) {
@@ -33,13 +82,34 @@ describe('Webdriver.io', function(){
       };
 
       client
-        .url("https://google.com")
         .execute(fn, 1, 2)
         .then(function(result){
           assert.equal(result.value, 3);
         })
         .call(done);
 
+    });
+  });
+
+  describe('when I execute JS code 100 times in one context',function(){
+    it('should return result 100 times', function(done){
+
+      fn = function(number1, number2) {
+        var result = number1 + number2;
+        return(result);
+      };
+
+      var i = 0;
+      while(i < 100){
+        i++;
+
+        client
+          .execute(fn, 1, 2)
+          .then(function(result){
+            assert.equal(result.value, 3);
+          })
+      }
+      client.call(done);
     });
   });
 });
