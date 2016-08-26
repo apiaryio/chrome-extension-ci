872bb8c42c70cfa2b5db664c5e855369af43af43
Adam Kliment <adam@apiary.io>
2016-08-26 11:53:52 +0200

    Http Request action
    * Http action WIP
    
    * HTTP action WIP
    
    * Removed unnecessary deps
    
    * HTTP optimistic case passing
    
    * Console.log cleanup
    
    * Removed debug
    
    * Passing http-request

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,8 +1,9 @@
 var echoController = require('./controllers/echo')
+var httpRequestController = require('./controllers/http-request')
 
 // The message dispatcher is meant to be plugged in as a listener
 // for background script's 'onMessageExternal' event.
-// see js/background.js
+// See: js/background.js
 //
 // It expects 'request' object as a first parameter with properties:
 // - action (string, required) the action name to dispatch
@@ -20,9 +21,10 @@ dispatcher = function (request, sender, sendResponse) {
   //Request is not an object
   if(typeof(request) !== 'object'){
     response = {
-      "error": {
-        "message": "Error: sent message is not an object"
-      }
+      error: {
+        message: "Error: sent message is not an object"
+      },
+      request: request
     }
     return(sendResponse(response));
   }
@@ -30,8 +32,10 @@ dispatcher = function (request, sender, sendResponse) {
   // Request has no action
   if(request['action'] === undefined){
     response = {
-      "error": {
-        "message": "Error: sent message object doesn't have the 'action' property"}
+      error: {
+        message: "Error: sent message object doesn't have the 'action' property"
+      },
+      request: request
     }
     return(sendResponse(response));
   }
@@ -39,18 +43,26 @@ dispatcher = function (request, sender, sendResponse) {
   // Connect controllers to actions
   switch(request['action']){
 
-    // Add your actions heress
+    // Add your actions here
     case "echo":
-      return echoController(request, sender, sendResponse)
+      echoController(request, sender, sendResponse)
+      break;
+
+
+    // ./src/controllers/http-request.js
+    case "httpRequest":
+      httpRequestController(request, sender, sendResponse)
+      break;
 
     // Unknown action controller
     default:
       response = {
-        "error": {
+        error: {
           "message": "Error: unknown action"
-        }
+        },
+        request: request
       }
-      return(sendResponse(response));
+      sendResponse(response);
   }
 }
 
